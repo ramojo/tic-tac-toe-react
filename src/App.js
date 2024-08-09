8eb7b0ae40cfe1cef188fa37b2d4b442c940b569
Allan <amramogo@gmail.com>
2024-08-09 23:35:19 +0300

    Adding useState

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
+import { useState } from 'react';
+
+function Square() {
+
+  const [value, setValue] = useState(null);
+  function handleClick() {
+    // console.log(e.target);
+    setValue('X');
+  }
+  return <button onClick={handleClick} className="square">{value}</button>;
+}
+
 export default function Board() {
   return (
   <>
     <div className="board-row">
-      <button className="square">1</button>
-      <button className="square">2</button>
-      <button className="square">3</button>
-      </div>
-      <div className="board-row">
-      <button className="square">4</button>
-      <button className="square">5</button>
-      <button className="square">6</button>
+        <Square />
+        <Square />
+        <Square />
     </div>
     <div className="board-row">
-      <button className="square">7</button>
-      <button className="square">8</button>
-      <button className="square">9</button>
+      <Square />
+      <Square />
+      <Square />
+    </div>
+    <div className="board-row">
+      <Square />
+      <Square />
+      <Square />
     </div>
     
     </>
   );
 }
+
+
